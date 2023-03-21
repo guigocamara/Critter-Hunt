c92a16a47b1cd2dddcc46140841dddf20de50750
Anna Zheng <anna_zheng@knights.ucf.edu>
2023-03-21 17:23:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ const url = process.env.MONGODB_URI;
 const mongoose = require("mongoose");
 mongoose.connect(url).then(() => console.log("mongodb connected")).catch(err => console.log(err));
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 var api = require('./api.js');
 api.setApp( app, mongoose );
 
