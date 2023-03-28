bd12c7c13c9027d384132818b3732e85b1db3afe
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2023-03-28 14:04:38 -0400

    redid formatting

--- api.js
+++ api.js
@@ -8,7 +8,74 @@ const User = require("./models/user.js");
 
 exports.setApp = function (app, client) {
 
-  /*
+  app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: username, password, favorite, error
+
+    var error = '';
+
+    const { username, password } = req.body;
+
+    //const db = client.db();
+    //const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+    const results = await User.find({ username: username, password: password });
+
+    var us = '';
+    var pa = '';
+    var fa = '';
+    var ret;
+
+    if (results.length > 0) {
+      us = results[0].username;
+      pa = results[0].password;
+      fa = results[0].favorite;
+      try {
+        const token = require("./createJWT.js");
+        ret = token.createToken(us, pa, fa);
+      }
+      catch (e) {
+        ret = { error: e.message };
+      }
+    }
+    else {
+      ret = { error: "Login/Password incorrect" };
+    }
+
+    res.status(200).json(ret);
+  });
+
+
+  app.post('/api/signUp', async (req, res, next) => {
+
+    var error = '';
+
+    const { username, password, favorite } = req.body;
+
+    const existingUser = await User.findOne({ username });
+
+    if(existingUser)
+    {
+      return res.status(400).json({message: 'usernmane already take. Please try another one!'});
+    }
+
+    else
+    {
+      const newUser = new User({username: username, password: password, favorite: favorite});
+      try
+      {    
+        await newUser.save();
+        const token = require('./createJWT.js');
+        const ret = token.createToken(username, password, favorite);
+        res.status(200).json(ret);
+      }
+      catch (e)
+      {
+        ret = { error: e.message };
+      }
+    }
+  });
+
+
   app.post('/api/addcard', async (req, res, next) =>
   {
     // incoming: userId, color
@@ -17,8 +84,57 @@ exports.setApp = function (app, client) {
     let token = require('./createJWT.js');
     const { userId, card, jwtToken } = req.body;
 
-    
+    try
+      {
+        if( token.isExpired(jwtToken))
+        {
+          var r = {error:'The JWT is no longer valid', jwtToken: ''};
+          res.status(200).json(r);
+          return;
+        }
+    }
+    catch(e)
+    {
+      console.log(e.message);
+    }
 
+    //const newCard = { Card: card, UserId: userId };
+    const newCard = new Card({ Card: card, UserId: userId });
+    var error = '';
+    try 
+    {
+      // const db = client.db();
+      // const result = db.collection('Cards').insertOne(newCard);
+      newCard.save();
+    }
+      catch (e) 
+    {
+      error = e.toString();
+    }
+    cardList.push( card );
+
+    var refreshedToken = null;
+    try
+    {
+      refreshedToken = token.refresh(jwtToken);
+    }
+    catch(e)
+    {
+      console.log(e.message);
+    }
+    
+    var ret = { error: error, jwtToken: refreshedToken };
+    res.status(200).json(ret);
+  });
+
+/*
+app.post('/api/addcard', async (req, res, next) =>
+  {
+    // incoming: userId, color
+    // outgoing: error
+
+    let token = require('./createJWT.js');
+    const { userId, card, jwtToken } = req.body;
 
     try
       {
@@ -63,132 +179,6 @@ exports.setApp = function (app, client) {
     res.status(200).json(ret);
   });
   */
-
-
-
-  /* our old api for login from main
-  app.post('/api/login', async (req, res, next) => 
-  {
-    // incoming: username, password
-    // outgoing: username, password, favorite, error
-    var error = '';
-    console.log(req.body);
-    const { username, password } = req.body;
-    const db = client.db("CritterHunt");
-    const results = await db.collection('Users').find({username:username,password:password}).toArray();
-    var id = '';
-    var fn = '';
-    var ln = '';
-    
-    if( results.length > 0 )
-    {
-      id = results[0].username;
-      fn = results[0].password;
-      ln = results[0].favorite;
-    }
-    var ret = { username:id, password:fn, favorite:ln, error:''};
-    res.status(200).json(ret);
-  });
-  */
-
-  //new login api
-  app.post('/api/login', async (req, res, next) => {
-    // incoming: login, password
-    // outgoing: username, password, favorite, error
-
-    var error = '';
-
-    const { username, password } = req.body;
-
-    //const db = client.db();
-    //const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
-    const results = await User.find({ username: username, password: password });
-
-    var us = '';
-    var pa = '';
-    var fa = '';
-    var ret;
-
-    if (results.length > 0) {
-      us = results[0].username;
-      pa = results[0].password;
-      fa = results[0].favorite;
-      try {
-        const token = require("./createJWT.js");
-        ret = token.createToken(us, pa, fa);
-      }
-      catch (e) {
-        ret = { error: e.message };
-      }
-    }
-    else {
-      ret = { error: "Login/Password incorrect" };
-    }
-
-    res.status(200).json(ret);
-  });
-
-  app.post('/api/signUp', async (req, res, next) => {
-
-  var error = '';
-
-  const { username, password, favorite } = req.body;
-
-  const existingUser = await User.findOne({ username });
-
-  if(existingUser)
-  {
-    return res.status(400).json({message: 'usernmane already take. Please try another one!'});
-  }
-
-  else
-  {
-    const newUser = new User({username: username, password: password, favorite: favorite});
-    try
-    {    
-      await newUser.save();
-      const token = require('./createJWT.js');
-      const ret = token.createToken(username, password, favorite);
-      res.status(200).json(ret);
-    }
-    catch (e)
-    {
-      ret = { error: e.message };
-    }
-  }
-});
-
-/*
- app.post('/api/signUp', async (req, res) =>
-  {
-    try 
-    {
-      const client = await MongoClient.connect(url);
-      
-      
-      const {username,password } = req.body;
-  
-      const existingUser = await User.findOne({ $or: [{username}]});
-
-      if(existingUser)
-      {
-        return res.status(400).send({message: 'Username taken. try again'});
-      }
-  
-      const newUser = {username, password};
-      const results = await User.insertOne(newUser);
-      
-      res.status(200).send({message: 'new user succesfully created', userId: results.insertedId});
-    }
-    catch (err)
-    {
-      console.log(err);
-      res.status(500).send({message: 'error'});
-    }
-  });
-*/
-
-
 
   /*
   app.post('/api/searchcards', async (req, res, next) => 
