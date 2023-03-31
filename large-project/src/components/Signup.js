fe0d03dd08354f462ae4e821b40d0a851b808767
Tony <93959678+MrT000@users.noreply.github.com>
2023-03-30 22:03:40 -0400

    Update Signup.js

--- large-project/src/components/Signup.js
+++ large-project/src/components/Signup.js
@@ -48,63 +48,7 @@ function Signup() {
             });
     }
     return (
-
-    <div className="Main-container">
-        <div className="container-login">
-          <span className="page-title">Hunt Critter with Critter Hunt!</span>
-            <div className="wrap-login">
-                <div className="login-pic">
-                    <img src="ProfilePictureMaker.png" alt="IMG"> </img>
-                </div>
-
-                <form className="login-form">
-                    <span className="login-form-title">Sign up</span>
-
-                    <div className="wrap-input">
-                        <input type="text" className="input" name="Username" id="signUpPassword" placeholder="Username" required>
-                        <span className="focus-input"></span>
-                        <span className="symbol-input">
-                            <i className="fa fa-user" aria-hidden="true"></i>
-                        </span> </input>
-                    </div>
-                    <div className="wrap-input">
-                        <input type="text" className="input" name="Email" placeholder="Email" required>
-                        <span className="focus-input"></span>
-                        <span className="symbol-input">
-                            <i className="fa fa-envelope" aria-hidden="true"></i>
-                        </span> </input>
-                    </div>
-
-                    <div className="wrap-input">
-                        <input type="password" className="input" name="pass" id="signUpPassword" placeholder="Password" required>
-                        <span className="focus-input"></span>
-                        <span className="symbol-input">
-                            <i className="fa fa-lock" aria-hidden="true"></i>
-                        </span>  </input>
-                    </div>
-                    <div className="wrap-input">
-                        <input type="text" className="input" name="favoriteAnimal" id="favoriteAnimal" placeholder="Favorite critter" required>
-                        <span className="focus-input"></span>
-                        <span className="symbol-input">
-                            <i className="fa fa-github-alt" aria-hidden="true"></i>
-                        </span> </input>
-                    </div>
-
-                    <div className="login-form-btn-container">
-                        <button className="login-form-btn">Sign up!</button>
-                    </div>
-
-                    <span id="signUpResult">{message}</span>
-                  <div className="text-center p-t-2">
-                      <a href="#/" className="txt2">Have an Account? Go login <i className="fa fa-long-arrow-right " aria-hidden="true"></i></a>
-                  </div>
-
-                </form>
-
-            </div>
-        </div>
-    </div>
-
+      
 
 
     );
