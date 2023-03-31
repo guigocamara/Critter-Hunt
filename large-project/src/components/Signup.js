cef9eebb1f0284debf010d8973b22921dc0961e1
Tony <93959678+MrT000@users.noreply.github.com>
2023-03-30 21:02:56 -0400

    loginssss

--- large-project/src/components/Signup.js
+++ large-project/src/components/Signup.js
@@ -55,7 +55,7 @@ function Signup() {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="style.css"> </link>
       <title>Critter Hunt Signup</title>
      </head>
 
