c4008dac750261b15ad979ce177d7978753118d2
Tony <93959678+MrT000@users.noreply.github.com>
2023-03-30 21:25:43 -0400

    login

--- large-project/src/components/Login.js
+++ large-project/src/components/Login.js
@@ -49,7 +49,7 @@ function Login() {
     return (
 
     <meta charset="UTF-8"> </meta>
-    <>...</>? <meta http-equiv="X-UA-Compatible" content="IE=edge"> </meta>
+    <>...</> <meta http-equiv="X-UA-Compatible" content="IE=edge"> </meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
     <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css"> </link>
