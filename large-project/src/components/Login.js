ef12ad4fd634b0a84646099aef86cc060eb7af1a
Tony <93959678+MrT000@users.noreply.github.com>
2023-03-30 21:28:22 -0400

    logidnsf

--- large-project/src/components/Login.js
+++ large-project/src/components/Login.js
@@ -48,12 +48,7 @@ function Login() {
     }
     return (
 
-    <meta charset="UTF-8"> </meta>
-    <>...</> <meta http-equiv="X-UA-Compatible" content="IE=edge"> </meta>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
-    <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css"> </link>
-    <title>Critter Hunt Login</title>
+
 
           <div className="Main-container">
               <div className="container-login">
--- large-project/src/components/Signup.js
+++ large-project/src/components/Signup.js
@@ -49,14 +49,6 @@ function Signup() {
     }
     return (
 
-
-      <meta charset="UTF-8"> </meta>
-      <>...</>? <meta http-equiv="X-UA-Compatible" content="IE=edge"> </meta>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
-      <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="style.css"> </link>
-      <title>Critter Hunt Signup</title>
-
     <div className="Main-container">
         <div className="container-login">
           <span className="page-title">Hunt Critter with Critter Hunt!</span>
