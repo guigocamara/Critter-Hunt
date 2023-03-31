11559471a4e416d5a76669d1346c311b7a0ffb46
Tony <93959678+MrT000@users.noreply.github.com>
2023-03-30 21:48:43 -0400

    logins

--- large-project/src/components/Headers.js
+++ large-project/src/components/Headers.js
@@ -3,11 +3,11 @@ function Header() {
   return (
 
     <div>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8"> </meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> </meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
     <script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css"> </link>
     <title>Critter Hunt</title>
     </div>
 
