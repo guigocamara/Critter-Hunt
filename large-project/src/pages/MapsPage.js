f2da979e895406b967d3c56cf462829491d0ddf8
dailenking <dailenking@knights.ucf.edu>
2023-04-02 16:42:25 -0400

    maps page

---
+++ large-project/src/pages/MapsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleMap from '../components/GoogleMap';
+
+
+const MapsPage = () =>
+{
+    return(
+      <div>
+         <GoogleMap />
+      </div>
+    );
+};
+
+export default MapsPage;
