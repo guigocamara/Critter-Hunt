5ebbbce0d0590b471ea8dc8b917a618e0c635f01
critter hunt <dailenking@knights.ucf.edu>
2023-04-03 19:20:10 -0400

    new commit

---
+++ large-project/src/components/MapSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import './map.css'
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
+
+const Map = ({ location, zoomLevel }) => (
+    <div className="map">
+      <h2 className="map-h2">View your critters!</h2>
+  
+      <div className="google-map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDJfd1QdAaC7kcziP2wamEwZ5od_o7xCRU' }}
+          defaultCenter={location}
+          defaultZoom={zoomLevel}
+        >
+          <LocationPin
+            lat={location.lat}
+            lng={location.lng}
+            text={location.address}
+          />
+        </GoogleMapReact>
+      </div>
+    </div>
+)
+
+const LocationPin = ({ text }) => (
+    <div className="pin">
+        <Icon icon={locationIcon} className="pin-icon" />
+            <p className="pin-text">{text}</p>
+    </div>
+)
+
+export default Map
