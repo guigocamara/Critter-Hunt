41026730e36465273ff0135422173fe10990652d
dailenking <dailenking@knights.ucf.edu>
2023-04-03 19:28:36 -0400

    random

--- large-project/src/pages/CardPage.js
+++ large-project/src/pages/CardPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PageTitle from '../components/PageTitle';
 import LoggedInName from '../components/LoggedInName';
 import CardUI from '../components/CardUI';
-import Map from '../components/Map';
 const CardPage = () =>
 {
     return(
@@ -10,7 +9,6 @@ const CardPage = () =>
             <PageTitle />
             <LoggedInName />
             <CardUI />
-            <Map />
         </div>
     );
 }
