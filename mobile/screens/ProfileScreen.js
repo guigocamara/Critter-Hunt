6840213accbf3f531eb8434d0cc8408a61f36361
guigocamara <guiadebola2@gmail.com>
2023-04-06 15:59:19 -0400

    Profile screen changes

--- mobile/screens/ProfileScreen.js
+++ mobile/screens/ProfileScreen.js
@@ -1,24 +1,44 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, } from 'react-native';
-import React, { useState, useEffect } from 'react';
-
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Profile({ route, navigation }) {
-    
-
-    return (
-        <View style={styles.container}>
-            <Text>Profile screen</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.profileImage}
+        source={require('mobile/assets/kermit.jpg')} // Updated image file name
+      />
+      <Text style={styles.text}>{"\n"}Number of critters caught:</Text>
+      <Text style={styles.text}>{"\n"}Critter Hunter since:</Text>
+      <Text style={styles.text}>{"\n"}Current rank:</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
+      backgroundColor: 'green',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      paddingTop: 70, 
     },
-});
+    imageContainer: {
+      marginTop: 0,
+    },
+    profileImage: {
+      width: 150,
+      height: 150,
+      borderRadius: 75,
+    },
+    textContainer: {
+      marginTop: 10,
+    },
+    text: {
+      fontSize: 15,
+      color: 'white',
+      fontWeight: 'bold',
+    },
+  });
