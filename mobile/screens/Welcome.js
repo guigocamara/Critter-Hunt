3614a732aa82782ed504057e8eda7498bd886242
guigocamara <guiadebola2@gmail.com>
2023-04-06 15:29:03 -0400

    updating my branch

--- mobile/screens/Welcome.js
+++ mobile/screens/Welcome.js
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
 
 
-export default function Welcome({ route, navigation }) {
-    const { username } = route.params;
+export default function Welcome({ navigation }) {
+    const [username, setUsername] = useState('');
+
+    const getUsername = async () => {
+        let result = await SecureStore.getItemAsync('userData');
+        if (result) {
+            let userData = JSON.parse(result);
+            setUsername(userData.username);
+        } else {
+            setUsername('');
+        }
+    }
+
+    const doLogout = async () => {
+        await SecureStore.deleteItemAsync('userData');
+        navigation.navigate('Login');
+    }
+
+    // when this screen renders, get the username from secure store and display
+    useEffect(() => {
+        getUsername();
+    }, [])
 
     return (
         <View style={styles.container}>
             <Text>Welcome, {username}</Text>
-            <Button title="Log out" onPress={() => navigation.navigate('Login')} />
+            <Button title="Log out" onPress={() => doLogout()} />
             <Button title="Go to camera" onPress={() => navigation.navigate('Camera')} />
             <Button title="Go to profile" onPress={() => navigation.navigate('Profile')} />
             <StatusBar style="auto" />
