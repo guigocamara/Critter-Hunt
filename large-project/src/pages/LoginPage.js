15f7f40d5754c3110516241e553ce70fb61bcbee
Tony <93959678+MrT000@users.noreply.github.com>
2023-04-06 19:51:24 -0400

    Update LoginPage.js

--- large-project/src/pages/LoginPage.js
+++ large-project/src/pages/LoginPage.js
@@ -8,8 +8,29 @@ const LoginPage = () =>
     return(
 
       <div>
+        <Headers />
         <PageTitle />
-        <Login/>
+        <div className="Main-container"> 
+          <div className="container-login"> 
+            <span className="page-title">Hunt Critter with Critter Hunt!</span>
+            <div className="wrap-login">
+              <div className="login-pic">
+                <img src="ProfilePictureMaker.png" alt="IMG"> </img> 
+              </div>
+              <a className="login-form">
+                <span className="login-form-title">Login</span>
+                <Login/>
+                <div className="text-center p-t-1">
+                  <span className="txt1">Forgot</span>
+                  <a className="txt2"> Username / Password ?</a>
+                </div>
+                <div className="text-center p-t-2">
+                  <a className="txt2">Create Your Account <i className="fa fa-long-arrow-right " aria-hidden="true"></i></a>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
         <Signup />
       </div>
     );
