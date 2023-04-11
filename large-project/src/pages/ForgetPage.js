2bda53c69dcd338ff3458d852bc2ce10a4d8b9d7
Tony <93959678+MrT000@users.noreply.github.com>
2023-04-11 17:36:51 -0400

    Update ForgetPage.js

--- large-project/src/pages/ForgetPage.js
+++ large-project/src/pages/ForgetPage.js
@@ -17,15 +17,7 @@ const ForgetPage = () =>
               <span className="login-form-title">Did you forget your password?</span>
               <span className="login-form-title">Enter your email</span>
 
-                <div className="wrap-input">
-                    <input type="text" class="input" name="Email" placeholder="Email" required> </input>
-                    <span class="focus-input"></span>
-                    <span class="symbol-input"> <i class="fa fa-envelope" aria-hidden="true"></i> </span>
-                </div>
                 
-                <div className="login-form-btn-container">
-                  <button className="login-form-btn" >Send Email Token</button>
-                </div>
 
                 <div className="text-center p-t-1">
                   <span className="txt1">Have an account?</span>
