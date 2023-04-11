5d460fe9bf6e2bc561f9b5a066b6993a438a8ac7
Tony <93959678+MrT000@users.noreply.github.com>
2023-04-11 17:16:59 -0400

    ForgetPage

--- large-project/src/App.js
+++ large-project/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import ForgetPage from './pages/ForgetPage';
 import CardPage from './pages/CardPage';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" index element={<LoginPage />} />
       <Route path="/Signup" index element={<SignupPage />} />
+      <Route path="/Signup" index element={<ForgetPage />} />
       <Route path="/cards" index element={<CardPage />} />
 
     </Routes>
