d8e82785129094d1d16a89c0b089881d3c6fcfdc
Tony <93959678+MrT000@users.noreply.github.com>
2023-04-10 21:56:44 -0400

    Loginss

--- large-project/src/pages/LoginPage.js
+++ large-project/src/pages/LoginPage.js
@@ -10,7 +10,22 @@ const LoginPage = () =>
         <div className="Main-container">
           <div className="container-login">
             <span className="page-title">Hunt Critter with Critter Hunt!</span>
-            <Login/>
+            <div class="wrap-login">
+              <a class="login-form">
+                <Login/>
+
+
+                <div class="text-center p-t-1">
+                  <span class="txt1">Forgot</span>
+                  <a href="#" class="txt2"> Username / Password ?</a>
+                </div>
+
+                <div class="text-center p-t-2">
+                  <a href="/Signup" class="txt2">Create Your Account <i class="fa fa-long-arrow-right " aria-hidden="true"></i></a>
+                </div>
+              </a>
+            </div>
+
           </div>
         </div>
         <Signup />
