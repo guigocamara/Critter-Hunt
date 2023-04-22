be49051d82672422e4b7983a149a45f1bb44dfd5
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2023-04-22 17:13:38 -0400

    Adding final push

--- large-project/src/components/AllPosts.js
+++ large-project/src/components/AllPosts.js
@@ -6,30 +6,30 @@ export default function AllPosts({postsList}){
     var bp = require('../components/Path.js');
     let [currentPostLikes, setCurrentPostLikes] = useState(0);
     let [currentPostId, setCurrentPostId] = useState("");
+    let [currentPostComments, setCurrentPostComments] = useState([]);
 
-    const updatePost = async event => {
-        var obj = { postsId: currentPostId, newLikes: currentPostLikes, newComments: [] };
-        var storage = require('../tokenStorage.js');
-        var js = JSON.stringify(obj);
-        
-        try {
-            const response = await fetch(bp.buildPath('api/updatepost'),
-                { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
-            var txt = await response.text();
-            var res = JSON.parse(txt);
-            if (res.error && res.error.length > 0) {
-                console.log("error");
-            }
-            else {
-                console.log("success");
-                storage.storeToken(res.jwtToken);
-            }
-        }
-        catch {
-            //console.log(e.toString());
-            console.log("error");
-        }
-    }
+    // const updatePost = async event => {
+    //     var obj = { postsId: currentPostId, newLikes: currentPostLikes, newComments: currentPostComments };
+    //     //var storage = require('../tokenStorage.js');
+    //     var js = JSON.stringify(obj);
+    //     try {
+    //         const response = await fetch(bp.buildPath('api/updatepost'),
+    //             { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+    //         console.log(response);
+    //         var txt = await response.text();
+    //         var res = JSON.parse(txt);
+    //         if (res.error && res.error.length > 0) {
+    //             console.log("error1");
+    //         }
+    //         else {
+    //             console.log("success");
+    //         }
+    //     }
+    //     catch {
+    //         //console.log(e.toString());
+    //         console.log("error2");
+    //     }
+    // }
 
 
     return(
@@ -38,7 +38,7 @@ export default function AllPosts({postsList}){
 
         {postsList.map(post => {
             return(
-            <div className="m-5 h-full w-80 bg-white rounded">
+            <div key={post._id} className="m-5 h-full w-80 bg-white rounded">
                 <div className="h-10 text-sm mt-3 ml-3 mr-3"> 
                     <div>{post.crittername}</div>
                     <div>{}</div>
@@ -47,7 +47,7 @@ export default function AllPosts({postsList}){
                 <img className="w-80 h-80" src={`http://critterhunt.herokuapp.com/image/${post.picture}`}/>
     
                 <div className="h-15 text-sm mb-3 ml-3 mr-3 flex items-center">
-                    <div className="flex flex-col items-center" onClick={() => {setCurrentPostId(post._id); setCurrentPostLikes(post.likes++); updatePost()}}>
+                    <div className="flex flex-col items-center" onClick={() => {setCurrentPostId(post._id); setCurrentPostLikes(post.likes++); /*updatePost()*/}}>
                         <Icon className="h-10 w-10" icon="material-symbols:star-rounded"/>
                         <div>{post.likes}</div>
                     </div>
