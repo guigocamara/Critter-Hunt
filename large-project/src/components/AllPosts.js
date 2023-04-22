7e47f4ee0c4a8230cfdc5b858cd92a6ec4268a2b
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2023-04-22 11:11:59 -0400

    Starting the work

--- large-project/src/components/AllPosts.js
+++ large-project/src/components/AllPosts.js
@@ -4,26 +4,30 @@ import { Icon } from '@iconify/react'
 
 export default function AllPosts({postsList}){
     var bp = require('../components/Path.js');
+    let [currentPostLikes, setCurrentPostLikes] = useState(0);
+    let [currentPostId, setCurrentPostId] = useState("");
 
-    const addLikes = async event => {
-        event.preventDefault();
+    const updatePost = async event => {
+        var obj = { postsId: currentPostId, newLikes: currentPostLikes, newComments: [] };
         var storage = require('../tokenStorage.js');
-        var obj = { userId: userId, card: card.value, jwtToken: storage.retrieveToken() };
         var js = JSON.stringify(obj);
+        
         try {
             const response = await fetch(bp.buildPath('api/updatepost'),
                 { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
             var txt = await response.text();
             var res = JSON.parse(txt);
             if (res.error && res.error.length > 0) {
-                setMessage("API Error:" + res.error);
+                console.log("error");
             }
             else {
-                
+                console.log("success");
+                storage.storeToken(res.jwtToken);
             }
         }
-        catch (e) {
-            setMessage(e.toString());
+        catch {
+            //console.log(e.toString());
+            console.log("error");
         }
     }
 
@@ -43,7 +47,7 @@ export default function AllPosts({postsList}){
                 <img className="w-80 h-80" src={`http://critterhunt.herokuapp.com/image/${post.picture}`}/>
     
                 <div className="h-15 text-sm mb-3 ml-3 mr-3 flex items-center">
-                    <div className="flex flex-col items-center" onClick={addLikes(post._id, post.likes)}>
+                    <div className="flex flex-col items-center" onClick={() => {setCurrentPostId(post._id); setCurrentPostLikes(post.likes++); updatePost()}}>
                         <Icon className="h-10 w-10" icon="material-symbols:star-rounded"/>
                         <div>{post.likes}</div>
                     </div>
