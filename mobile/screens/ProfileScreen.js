f723049e29961ac65a4d355ff0b780d94ee0f70f
guigocamara <guiadebola2@gmail.com>
2023-04-26 00:56:09 -0400

    Displaying all posts in profile screen

--- mobile/screens/ProfileScreen.js
+++ mobile/screens/ProfileScreen.js
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, SafeAreaView, FlatList, Pressable, ScrollView } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
 async function getItem(key) {
   return await SecureStore.getItemAsync(key);
 }
 
+const Item = ({ title, picture, navigation, postID }) => (
+  <Pressable onPress={() => navigation.navigate('PostDetails', { postID: postID })}>
+    <View style={styles.item}>
+      <Image
+        style={styles.image}
+        source={{ uri: 'http://critterhunt.herokuapp.com/image/' + picture }}
+      />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  </Pressable>
+);
+
 export default function Profile({ route, navigation }) {
   const [userId, setUserId] = useState('');
   const [dateJoined, setDateJoined] = useState('');
   const [rank, setRank] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
 
   const fetchData = async () => {
     const userId = await getItem('userId');
@@ -20,8 +33,31 @@ export default function Profile({ route, navigation }) {
     setDateJoined(data.dateJoined);
   };
 
+  // Fetch user's posts
+  const fetchUserPosts = async () => {
+    const userId = await getItem('userId');
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ search: "", jwtToken: "", userId: userId }),
+    };
+
+    try {
+      const response = await fetch('http://critterhunt.herokuapp.com/api/searchposts', requestOptions);
+      const data = await response.json();
+      if (data.error) {
+        console.error(data.error);
+      } else {
+        setUserPosts(data._ret);
+      }
+    } catch (error) {
+      console.error('Error fetching user posts:', error);
+    }
+  };
+
   useEffect(() => {
     fetchData();
+    fetchUserPosts();
   }, []);
 
   const formatDate = (dateString) => {
@@ -47,23 +83,45 @@ export default function Profile({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.profileImage}
-        source={require('mobile/assets/kermit.jpg')} // Updated image file name
-      />
-      <Text style={styles.text}>{"\n"}Number of critters caught:</Text>
-      <Text style={styles.text}>
-        {"\n"}Critter Hunter since: {dateJoined ? formatDate(dateJoined) : ''}
-      </Text>
-      {rank && <Text style={styles.text}>{"\n"}Current rank: {rank}</Text>}
+      <View style={styles.profileContent}>
+        <Image
+          style={styles.profileImage}
+          source={require('mobile/assets/kermit.jpg')}
+        />
+        <Text style={styles.text}>{"\n"}Number of critters caught:</Text>
+        <Text style={styles.text}>
+          {"\n"}Critter Hunter since: {dateJoined ? formatDate(dateJoined) : ''}
+        </Text>
+        {rank && <Text style={styles.text}>{"\n"}Current rank: {rank}</Text>}
+      </View>
+  
+      <Text style={styles.headerText}>{"\n"}Your Posts:</Text>
+      <SafeAreaView style={styles.postsContainer}>
+        <FlatList
+          style={styles.postList}
+          data={userPosts}
+          renderItem={({ item }) => (
+            <Item
+              title={item.crittername}
+              picture={item.picture}
+              navigation={navigation}
+              postID={item._id}
+            />
+          )}
+          keyExtractor={item => item._id}
+          numColumns={2}
+        />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#128c4b',
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -85,5 +143,37 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
+  headerText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  postsContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#128c4b',
+    width: '100%',
+    alignItems: 'center',
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    width: 155,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 145,
+    height: 145,
+  },
+  profileContent: {
+    alignItems: 'center',
+  },
 });
+
 
