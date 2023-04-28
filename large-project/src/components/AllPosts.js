9277696abba135a2d7a1fe8554b26e813971c2d3
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2023-04-28 16:18:17 -0400

    Adding likes to database

--- large-project/src/components/AllPosts.js
+++ large-project/src/components/AllPosts.js
@@ -4,32 +4,29 @@ import { Icon } from '@iconify/react'
 
 export default function AllPosts({postsList}){
     var bp = require('../components/Path.js');
-    let [currentPostLikes, setCurrentPostLikes] = useState(0);
-    let [currentPostId, setCurrentPostId] = useState("");
-    let [currentPostComments, setCurrentPostComments] = useState([]);
 
-    // const updatePost = async event => {
-    //     var obj = { postsId: currentPostId, newLikes: currentPostLikes, newComments: currentPostComments };
-    //     //var storage = require('../tokenStorage.js');
-    //     var js = JSON.stringify(obj);
-    //     try {
-    //         const response = await fetch(bp.buildPath('api/updatepost'),
-    //             { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
-    //         console.log(response);
-    //         var txt = await response.text();
-    //         var res = JSON.parse(txt);
-    //         if (res.error && res.error.length > 0) {
-    //             console.log("error1");
-    //         }
-    //         else {
-    //             console.log("success");
-    //         }
-    //     }
-    //     catch {
-    //         //console.log(e.toString());
-    //         console.log("error2");
-    //     }
-    // }
+    const updatePost = async (postId, postLikes, event) => {
+        console.log(postId);
+        var obj = { postsId: postId, newLikes: postLikes + 1, newComments: [] };
+        //var storage = require('../tokenStorage.js');
+        var js = JSON.stringify(obj);
+        try {
+            const response = await fetch(bp.buildPath('api/updatepost'),
+                { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+            console.log(response);
+            var txt = await response.text();
+            var res = JSON.parse(txt);
+            if (res.error && res.error.length > 0) {
+                //console.log("error1");
+            }
+            else {
+                //console.log("success");
+            }
+        }
+        catch {
+            //console.log("error2");
+        }
+    }
 
 
     return(
@@ -47,7 +44,7 @@ export default function AllPosts({postsList}){
                 <img className="w-80 h-80" src={`http://critterhunt.herokuapp.com/image/${post.picture}`}/>
     
                 <div className="h-15 text-sm mb-3 ml-3 mr-3 flex items-center">
-                    <div className="flex flex-col items-center" onClick={() => {setCurrentPostId(post._id); setCurrentPostLikes(post.likes++); /*updatePost()*/}}>
+                    <div className="flex flex-col items-center" onClick={() => {updatePost(post._id, post.likes)}}>
                         <Icon className="h-10 w-10" icon="material-symbols:star-rounded"/>
                         <div>{post.likes}</div>
                     </div>
